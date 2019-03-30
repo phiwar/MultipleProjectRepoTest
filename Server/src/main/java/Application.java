758fd9682cac98e326a5ab733748079975e5d803
Philip <philip.warfvinge@outlook.com>
2019-03-30 14:46:50 +0100

    Actually made it conform to style-guide

--- Server/src/main/java/Application.java
+++ Server/src/main/java/Application.java
@@ -2,21 +2,22 @@
  * Main application class.
  */
 public final class Application {
-    /**
-     * Private constructor.
-     * This class is not meant to be initialized after compile-time.
-     */
-    private Application() { }
-    /**
-     * Main method. Initializes Counter.
-     * @param args The arguments
-     */
-    public static void main(final String[] args) {
 
-        Counter model = new Counter(1);
-        model.inc(2);
-        System.out.println(model.getCount());
-    }
+  /**
+   * * Private constructor.
+   * This class is not meant to be initialized after compile-time.
+   */
+  private Application() { }
+
+  /**
+   * Main method. Initializes Counter.
+   * @param args The arguments
+   */
+  public static void main(final String[] args) {
+    Counter model = new Counter(1);
+    model.inc(2);
+    System.out.println(model.getCount());
+  }
 
 
 }
--- Server/src/main/java/Counter.java
+++ Server/src/main/java/Counter.java
@@ -5,34 +5,34 @@
 public class Counter {
 
 
-    /**
-     * Keeps track of the curent count of this counter.
-     */
-    private int count;
+  /**
+   * * Keeps track of the curent count of this counter.
+   */
+  private int count;
 
-    /**
-     * Initialize counter with default value.
-     * @param initial The initial value of the counter
-     */
-    public Counter(final int initial) {
-        this.count = initial;
-    }
+  /**
+   * * Initialize counter with default value.
+   * @param initial The initial value of the counter
+   */
+  public Counter(final int initial) {
+    this.count = initial;
+  }
 
-    /**
-     * Increment the counter by step.
-     * @param step An Integer step to increment the counter with
-     * @return the updated value, current count + step.
-     */
-    public final int inc(final int step) {
-        count = count + step;
-        return count;
-    }
+  /**
+   * Increment the counter by step.
+   * @param step An Integer step to increment the counter with
+   * @return the updated value, current count + step.
+   */
+  public final int inc(final int step) {
+    count = count + step;
+    return count;
+  }
 
-    /**
-     *
-     * @return The current count of the counter
-     */
-    public final int getCount() {
-        return count;
-    }
+  /**
+   *
+   * @return The current count of the counter.
+   */
+  public final int getCount() {
+    return count;
+  }
 }
